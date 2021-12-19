23224c01295368e2782900ae5f5f00af4898297b
+ <brandi.ude@gmail.com>
2021-12-19 18:56:53 -0500

    commit tree 44d6fa61e8c5aeb399f637637ef44600cf027146

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,13 +1,28 @@
 class ClientsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_message
+
+    def index 
+        render json: Client.all
+    end
+
     def show 
         client = Client.find(params[:id])
         render json: client       
     end
+    
+    def update
+        client = Client.find_by(id: params[:id])
+        client.update(client_params)
+        render json: gym
+      end
 
     private  
 
     def record_not_found_message
         render json: {error: "Client not found"}, status: :not_found
+       end
+
+       def client_params
+        permit.params(:name, :age)
        end
 end
--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,6 +1,10 @@
 class GymsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_message
 
+    def index 
+        render json: Gym.all
+    end
+
     def show 
         gym = Gym.find(params[:id])
         render json: gym       
@@ -12,10 +16,20 @@ class GymsController < ApplicationController
         head :no_content
     end
 
+    def update
+        gym = Gym.find_by(id: params[:id])
+        gym.update(gym_params)
+        render json: gym
+      end
+
     private  
 
     def record_not_found_message
         render json: {error: "Gym not found"}, status: :not_found
+       end
+
+       def gym_params
+        permit.params(:name, :address)
        end
 
 end
